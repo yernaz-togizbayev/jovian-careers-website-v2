aff5c3471476972d14239580c72308a8c7b25808
Yernaz Togizbayev <71610255+yernaz-togizbayev@users.noreply.github.com>
2023-03-26 17:32:11 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -3,16 +3,14 @@ from sqlalchemy import create_engine, text
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={
-    "ssl": {
-      "ssl_ca": "/etc/ssl/cert.pem"
-    }
-  }
-)
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 my_secret = os.environ['DB_CONNECTION_STRING']
+
+
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
@@ -22,3 +20,11 @@ def load_jobs_from_db():
     return jobs
 
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs WHERE id=:val"),
+                          {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    return dict(rows[0]._mapping)
