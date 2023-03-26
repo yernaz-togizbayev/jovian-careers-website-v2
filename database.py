afc1db8469d13de84f44f6520927a8d092ae4391
Yernaz Togizbayev <71610255+yernaz-togizbayev@users.noreply.github.com>
2023-03-26 15:34:50 +0000

    added connection to db

---
+++ database.py
@@ -0,0 +1,24 @@
+import os
+from sqlalchemy import create_engine, text
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  }
+)
+
+my_secret = os.environ['DB_CONNECTION_STRING']
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row._mapping))
+    return jobs
+
+
--- main.py
+++ main.py
@@ -1,44 +1,21 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 150,00,00'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 150,00,00'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'remote'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$120,000'
-  },
-  
-]
-
 @app.route("/")
-
-def hello_word():
+def hello_jovian():
+  jobs = load_jobs_from_db()
   return render_template('home.html',
-                         jobs=JOBS)
+                          jobs=jobs,
+                          company_name = 'Jovian') 
 
-
+  
 @app.route("/api/jobs")
-
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug = True)
+  app.run(host='0.0.0.0', debug=True)
